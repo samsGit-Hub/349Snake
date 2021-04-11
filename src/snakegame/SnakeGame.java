02a430dd934aa1e644400a16a4b43e590681117a
Sdvan <Sdvan@10.252.31.13>
2021-04-11 19:13:40 -0400

    Added the about button and made it functional

--- src/snakegame/SnakeGame.java
+++ src/snakegame/SnakeGame.java
@@ -4,13 +4,19 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import app.JApplication;
+import io.ResourceFinder;
 
 public class SnakeGame extends JApplication implements ActionListener
 {
@@ -18,6 +24,7 @@ public class SnakeGame extends JApplication implements ActionListener
   private JComboBox<String> snakeOp, landOp;
   private JButton start;
   private JLabel title;
+  private String aboutText = "";
   private static final String[] snakeCo = { "Snake", "Red", "Blue", "Yellow", "Purple", "Green" };
   private static final String[] landCo = { "Background", "Black", "White" , "Pink", "Checker Board" } ;
   public static final int WIDTH = 700;
@@ -49,18 +56,57 @@ public class SnakeGame extends JApplication implements ActionListener
     landOp.addActionListener(this);
     start = new JButton("START");
     start.addActionListener(this);
+    about = new JButton("ABOUT");
+    about.addActionListener(this);
     display.add(snakeOp);
     display.add(landOp);
     display.add(title);
+    display.add(about);
     display.add(start);
     mainPanel.add(display);
   }
 
   @Override
-  public void actionPerformed(ActionEvent e)
+  public void actionPerformed(ActionEvent evt)
   {
-    // TODO Auto-generated method stub
-    
+    if (evt.getSource().equals(start))
+    {
+      handleStart();
+    }
+    else if (evt.getSource().equals(about))
+    {
+      handleAbout();
+    }
   }
 
+  protected void handleStart()
+  {
+    
+  }
+  
+  protected void handleAbout()
+  {
+    ResourceFinder rf = ResourceFinder.createInstance(new resources.Marker());
+    InputStream is = rf.findInputStream("about.txt");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+    try
+    {
+      if (reader != null)
+      {
+        String aboutInfo = "";
+        while ((aboutInfo = reader.readLine()) != null)
+        {
+          aboutText += aboutInfo + "\n";
+        }
+      }
+      reader.close();
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+    JOptionPane.showMessageDialog(null, aboutText, "About the game", JOptionPane.INFORMATION_MESSAGE);
+    aboutText = "";
+  }
 }
